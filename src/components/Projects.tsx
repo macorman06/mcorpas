57c6ff0258f54fbd4b413189e44a655e4f1415d4
Marcos <48572597+macorman06@users.noreply.github.com>
2025-07-05 21:11:58 +0200

    Updated Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ExternalLink, Instagram, X, Github } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const Projects = () => {
   const [isOpen, setIsOpen] = useState(false);
