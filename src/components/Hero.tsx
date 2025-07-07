c8949ecb2b52dce241a62bc76616eda843375e1d
Marcos <48572597+macorman06@users.noreply.github.com>
2025-07-07 13:23:51 +0200

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,32 +9,33 @@ const Hero = () => {
   return (
     <section className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 via-purple-50 to-pink-50 dark:from-gray-900 dark:via-purple-900/20 dark:to-blue-900/20">
     <div className="w-full max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 pt-32 sm:pt-0 overflow-x-hidden">
-        <div className="mb-8 w-full">
-          <div class="flex flex-wrap md:flex-nowrap items-center gap-6">
-            <img
-              src="/profile/avatar_mcorpas.png"
-              alt="Marcos Corpas"
-              className="w-48 h-48 rounded-full"
-            />
-            <div className="text-left">
-              <h1 className="text-4xl md:text-6xl font-bold text-gray-900 dark:text-white">
-                Hey, soy{' '}
-                <span className="bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent">
-                  Marcos
-                </span>{' '}
-                ✨
-              </h1>
-              <h2 className="text-4xl md:text-6xl font-semibold text-gray-900 dark:text-white mt-4">
-                <span className="bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent">
-                  Ingeniero Aeroespacial
-                </span>
-              </h2>
-            </div>
+      <div className="mb-8 w-full">
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-6 text-center md:text-left">
+          <img
+            src="/profile/avatar_mcorpas.png"
+            alt="Marcos Corpas"
+            className="w-40 h-40 md:w-48 md:h-48 rounded-full mx-auto md:mx-0"
+          />
+          <div>
+            <h1 className="text-4xl md:text-6xl font-bold text-gray-900 dark:text-white">
+              Hey, soy{' '}
+              <span className="bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent">
+                Marcos
+              </span>{' '}
+              ✨
+            </h1>
+            <h2 className="text-3xl md:text-6xl font-semibold text-gray-900 dark:text-white mt-4">
+              <span className="bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent">
+                Ingeniero Aeroespacial
+              </span>
+            </h2>
           </div>
-          <p className="mt-6 text-lg md:text-xl text-gray-600 dark:text-gray-300 leading-relaxed text-left">
-            {t('hero.description')}
-          </p>
         </div>
+        <p className="mt-6 text-lg md:text-xl text-gray-600 dark:text-gray-300 leading-relaxed text-center md:text-left">
+          {t('hero.description')}
+        </p>
+      </div>
+
 
         <div className="flex items-center justify-left gap-12">
           {/* Botones */}
