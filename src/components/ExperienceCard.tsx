ccfad9f231f2aad3d59eb488739b53eb99439008
Marcos <48572597+macorman06@users.noreply.github.com>
2025-07-07 14:07:41 +0200

    Updated ExperienceCard.tsx

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -39,7 +39,7 @@ const ExperienceCard: React.FC<ExperienceCardProps> = ({
       </p>
 
       {/* Descripción */}
-      <p className="text-gray-600 dark:text-gray-300 mb-4">{description}</p>
+      <p className="text-gray-600 dark:text-gray-800 mb-4">{description}</p>
 
       {/* Tareas */}
       <ul className="space-y-2">
