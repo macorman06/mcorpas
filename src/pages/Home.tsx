b6c81a5c7ae662d785aaded7dd518a5775982348
Marcos <48572597+macorman06@users.noreply.github.com>
2025-07-07 12:48:37 +0200

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,7 @@ const Home = () => {
       </Helmet>
       <Hero />
       <Skills />
-      <div className="border-t border-gray-200 dark:border-gray-700 my-12 w-full"></div>
+      <div className="border-t border-gray-200 dark:border-gray-700 w-full"></div>
       <Experience />
 
     </>
