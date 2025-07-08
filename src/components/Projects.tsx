47cf923625b9c2082c095978e3d9f08578eb458d
Marcos <48572597+macorman06@users.noreply.github.com>
2025-07-08 12:07:48 +0200

    Updated Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ExternalLink, Instagram, X, Github, Filter } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import ProjectCardDetail from './ProjectCardDetail'; // Ruta correcta
+import ProjectCardDetail from 'src/components/ProjectsCardDetail.tsx'; // Ruta correcta
 
 export type ProjectStatus = 'En progreso' | 'Terminado' | 'Sin soporte';
 
